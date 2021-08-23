23412ab581705fc986d2670263603ae82303bdc0
waqar Ahmad <waqaar.ahmad@yahoo.com>
2021-08-23 11:50:15 +0500

    push 2nd build

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
 
 import './App.css';
-import Users from './Users';
-
-
 
 function App({name,age}) {
   return (
     <div className="App">
       <h1>Hello {name}</h1>
-      <span>His Age is {age}</span>
-      <br />
-      
-      <Users name="Umer" age={9}  />
-        
+      <span>His Age is {age}</span>   
     </div>
   );
 }
